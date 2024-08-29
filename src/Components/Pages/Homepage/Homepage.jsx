5c36725238e4621ba7cd0779d6e4dce7f5670d4a
ElGolden69 <kefisaif27@gmail.com>
2024-08-29 19:07:04 +0100

    added a new section

--- src/Components/Pages/Homepage/Homepage.jsx
+++ src/Components/Pages/Homepage/Homepage.jsx
@@ -6,12 +6,14 @@ import CardComponent from "../../Price/CardComponent";
 import CallToAction from "../../CallToAction/CallToAction";
 import Review from "../../Review/Review";
 import Footer from "../../Footer/Footer";
+import Slides from "../../Slides/Slides";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
       <Herosection/>
+      <Slides/>
       <Features/>
       <CardComponent/>
       <CallToAction/>
