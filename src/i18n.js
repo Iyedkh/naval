71f5d4f1df2c6c7f481dbb53a6bba2019ed2c8dd
Khouildi Iyed <iyedkhouildi12@gmail.com>
2024-09-01 22:14:06 +0100

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -14,7 +14,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en', // default language
+    lng: 'fr', // default language
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
